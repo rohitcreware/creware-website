e2580577f6de9529ad6898ac83e98050b53cab34
rohitcreware <Rohit.sharma@creware.asia>
2023-10-09 18:45:08 +0530

    error solved

---
+++ Json/WebApp.js
@@ -0,0 +1,33 @@
+import { Fifth, Four, One, Sixth, Three, Two } from "@/components/Svgs/mobileapp";
+export const firstCard = [
+    {
+      img: <Four/>,
+      head: "Act Faster",
+      desc: "Lorem ipsum dolor sit amet consectetur. Ipsum molestie pulvinar tempus sed tempus. Et pharetra gravida sed sit gravida. Id at morbi dignissim mauris. Vulputate ",
+    },
+    {
+      img: <Fifth/>,
+      head: "Reach More Audiences",
+      desc: "Lorem ipsum dolor sit amet consectetur. Ipsum molestie pulvinar tempus sed tempus. Et pharetra gravida sed sit gravida. Id at morbi dignissim mauris. Vulputate ",
+    },
+    {
+      img: <Sixth/>,
+      head: "Sell Your Services",
+      desc: "Lorem ipsum dolor sit amet consectetur. Ipsum molestie pulvinar tempus sed tempus. Et pharetra gravida sed sit gravida. Id at morbi dignissim mauris. Vulputate ",
+    },
+    {
+      img: <One/>,
+      head: "Mobile App Security",
+      desc: "Lorem ipsum dolor sit amet consectetur. Ipsum molestie pulvinar tempus sed tempus. Et pharetra gravida sed sit gravida. Id at morbi dignissim mauris. Vulputate ",
+    },
+    {
+      img: <Two/> ,
+      head: "Mobile App Maintance",
+      desc: "Lorem ipsum dolor sit amet consectetur. Ipsum molestie pulvinar tempus sed tempus. Et pharetra gravida sed sit gravida. Id at morbi dignissim mauris. Vulputate ",
+    },
+    {
+      img: <Three/>,
+      head: "Hybrid Mobile App",
+      desc: "Lorem ipsum dolor sit amet consectetur. Ipsum molestie pulvinar tempus sed tempus. Et pharetra gravida sed sit gravida. Id at morbi dignissim mauris. Vulputate ",
+    },
+  ];
